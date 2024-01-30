3bebe4f32dc388809f098b6009e362e97b2e102e
aref2910 <149997779+aref2910@users.noreply.github.com>
2024-01-30 05:56:20 +0330

    Update logger.c

--- Q2/logger.c
+++ Q2/logger.c
@@ -5,14 +5,37 @@
 
 void kernel_log(int level, int eventType, const char* message, 
                 const char* file, int line) {
-    // TODO: Implement the logging functionality here
+  FILE *log_file = fopen("txt.log_kernel", "a");  
+    if (log_file != NULL) {
+        fprintf(log_file, "[%s] [%s] %s at %s:%d\n", get_log_level_name(log_level), get_event_type_name(event_type), message, file_name, line);
+        fclose(log_file);  
+    } else {
+        fprintf(stderr, "Error opening or creating txt.log_kernel file.\n");
+    }
 }
-
 const char* get_log_level_name(int level) {
-    // TODO: Implement the get_log_level_name function
+    switch (log_level) {
+        case DEBUG_LOG:
+            return "DEBUG";
+        case INFO_LOG:
+            return "INFO";
+        case WARNING_LOG:
+            return "WARNING";
+        case ERROR_LOG:
+            return "ERROR";
+        default:
+            return "UNKNOWN_LOG";
+    }
 }
-
-
 const char* get_event_type_name(int eventType) {
-    // TODO: Implement the get_event_type_name function
+    switch (event_type) {
+        case SCHEDULING_PROCESS:
+            return "SCHEDULING_PROCESS";
+        case MEMORY_MANAGEMENT:
+            return "MEMORY_MANAGEMENT";
+        case IO_EVENT:
+            return "IO_EVENT";
+        default:
+            return "UNKNOWN_EVENT";
+    }
 }
